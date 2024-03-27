7ca7063980ee3b480f023413e2eb5a15f65d17a0
daudau2002 <dohongquan1505@gmail.com>
2024-03-27 09:10:35 +0700

    createOrder

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -9,42 +9,41 @@ import ManageUser from "../pages/ManageUser";
 import ManageProduct from "../pages/ManageProduct";
 import PrivateRoutes from "./PrivateRoutes";
 import Invoices from "../pages/invoice/Invoices";
+import CreateOrder from "../pages/order/CreateOrder";
 
 const AppRoutes = (props) => {
-    return (
-        <>
-            <Routes>
-                <Route path="/" element={<Home />}></Route>
-                <Route path="/login" element={<Login />}></Route>
-                <Route
-                    path="/detail-product/:id"
-                    element={<DetailProduct />}
-                ></Route>
-                <Route
-                    path="/manage-customer"
-                    element={
-                        <PrivateRoutes>{<ManageCustomer />}</PrivateRoutes>
-                    }
-                />
-                <Route
-                    path="/manage-user"
-                    element={<PrivateRoutes>{<ManageUser />}</PrivateRoutes>}
-                />
-                <Route
-                    path="/manage-product"
-                    element={<PrivateRoutes>{<ManageProduct />}</PrivateRoutes>}
-                />
-                <Route
-                    path="/detail-customer/:userId"
-                    element={
-                        <PrivateRoutes>{<DetailCustomer />}</PrivateRoutes>
-                    }
-                />
-                <Route path='/invoices' element={<Invoices/>}/>
-                <Route path="/*" element={<NotFoundPage />}></Route>
-            </Routes>
-        </>
-    );
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/detail-product/:id" element={<DetailProduct />}></Route>
+        <Route
+          path="/manage-customer"
+          element={<PrivateRoutes>{<ManageCustomer />}</PrivateRoutes>}
+        />
+        <Route
+          path="/manage-user"
+          element={<PrivateRoutes>{<ManageUser />}</PrivateRoutes>}
+        />
+        <Route
+          path="/manage-product"
+          element={<PrivateRoutes>{<ManageProduct />}</PrivateRoutes>}
+        />
+        <Route
+          path="/detail-customer/:userId"
+          element={<PrivateRoutes>{<DetailCustomer />}</PrivateRoutes>}
+        />
+        <Route path="/invoices" element={<Invoices />} />
+        <Route path="/orders" element={<CreateOrder />} />
+        {/* <Route
+          path="/orders"
+          element={<PrivateRoutes>{<CreateOrder />}</PrivateRoutes>}
+        /> */}
+        <Route path="/*" element={<NotFoundPage />}></Route>
+      </Routes>
+    </>
+  );
 };
 
 export default AppRoutes;
